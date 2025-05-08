a5e017455e2f6471a6c0ddecb61defea09cec149
Michał Zimka <michal@zimka.pl>
2025-05-08 19:57:50 +0200

    Pierwsze dwa endpointy

--- Tutorial8/Models/DTOs/TripDTO.cs
+++ Tutorial8/Models/DTOs/TripDTO.cs
@@ -4,6 +4,10 @@ public class TripDTO
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Opis { get; set; }
+    public DateTime DataOd { get; set; }
+    public DateTime DataDo { get; set; }
+    public int ileOsob { get; set; }
     public List<CountryDTO> Countries { get; set; }
 }
 
--- Tutorial8/Services/TripsService.cs
+++ Tutorial8/Services/TripsService.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
 using Tutorial8.Models.DTOs;
 
 namespace Tutorial8.Services;
 
 public class TripsService : ITripsService
 {
-    private readonly string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=APBD;Integrated Security=True;";
-    
+    private readonly string _connectionString =
+        "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=APBD;Integrated Security=True;";
+
     public async Task<List<TripDTO>> GetTrips()
     {
         var trips = new List<TripDTO>();
 
-        string command = "SELECT IdTrip, Name FROM Trip";
-        
+        string command = @"
+            SELECT 
+                t.IdTrip, t.Name, t.Description, t.DateFrom, t.DateTo, t.MaxPeople,
+                c.Name AS CountryName
+            FROM Trip t
+                LEFT JOIN Country_Trip ct ON t.IdTrip = ct.IdTrip
+                LEFT JOIN Country c ON ct.IdCountry = c.IdCountry
+            ORDER BY t.IdTrip;
+        ";
+
         using (SqlConnection conn = new SqlConnection(_connectionString))
         using (SqlCommand cmd = new SqlCommand(command, conn))
         {
@@ -23,16 +33,89 @@ public class TripsService : ITripsService
                 while (await reader.ReadAsync())
                 {
                     int idOrdinal = reader.GetOrdinal("IdTrip");
-                    trips.Add(new TripDTO()
+                    var current = new TripDTO()
                     {
                         Id = reader.GetInt32(idOrdinal),
                         Name = reader.GetString(1),
-                    });
+                        Opis = reader.GetString(2),
+                        DataOd = reader.GetDateTime(3),
+                        DataDo = reader.GetDateTime(4),
+                        ileOsob = reader.GetInt32(5),
+                        Countries = new List<CountryDTO>()
+                    };
+                    
+                    trips.Add(current);
+                    
+                    if (!reader.IsDBNull(6))
+                    {
+                        current.Countries.Add(new CountryDTO()
+                        {
+                            Name = reader.GetString(6)
+                        });
+                    }
+                }
+            }
+        }
+
+
+        return trips;
+    }
+
+    public async Task<List<ClientTripDTO>> GetClientTrips(int idClient)
+    {
+        var trips = new List<ClientTripDTO>();
+
+        var query = @"
+            SELECT 
+                t.IdTrip, t.Name, t.Description, t.DateFrom, t.DateTo, t.MaxPeople,
+                ct.RegisteredAt, ct.PaymentDate,
+                c.Name AS CountryName
+            FROM Client_Trip ct
+                JOIN Trip t ON ct.IdTrip = t.IdTrip
+                LEFT JOIN Country_Trip ctr ON t.IdTrip = ctr.IdTrip
+                LEFT JOIN Country c ON ctr.IdCountry = c.IdCountry
+            WHERE ct.IdClient = @IdClient
+            ORDER BY t.IdTrip;
+        ";
+
+        using (var conn = new SqlConnection(_connectionString))
+        using (var cmd = new SqlCommand(query, conn))
+        {
+            cmd.Parameters.AddWithValue("@IdClient", idClient);
+            await conn.OpenAsync();
+
+            using (var reader = await cmd.ExecuteReaderAsync())
+            {
+
+                while (await reader.ReadAsync())
+                {
+                    int idTrip = reader.GetInt32(reader.GetOrdinal("IdTrip"));
+
+                    var current = new ClientTripDTO()
+                    {
+                        IdTrip = idTrip,
+                        Name = reader.GetString(1),
+                        Description = reader.GetString(2),
+                        DateFrom = reader.GetDateTime(3),
+                        DateTo = reader.GetDateTime(4),
+                        MaxPeople = reader.GetInt32(5),
+                        RegisteredAt = reader.GetDateTime(6),
+                        PaymentDate = reader.IsDBNull(7) ? null : reader.GetDateTime(7),
+                        Countries = new List<CountryDTO>()
+                    };
+                    trips.Add(current);
+
+                    if (!reader.IsDBNull(8))
+                    {
+                        current.Countries.Add(new CountryDTO()
+                        {
+                            Name = reader.GetString(8)
+                        });
+                    }
                 }
             }
         }
         
-
         return trips;
     }
 }
